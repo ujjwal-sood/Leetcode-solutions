215fcc33c55c8a9026ff3ccb49959c0e65b7fdcb
Ujjwal Sood <77429111+ujjwalsood95@users.noreply.github.com>
2021-02-03 15:16:07 +0530

    Update number-of-islands.py

--- number-of-islands.py
+++ number-of-islands.py
@@ -2,7 +2,7 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n=len(grid),len(grid[0])
         
-        def f(arr,row,col,flag):
+        def f(arr,row,col):
             if row<0 or col<0 or row>=m or col>=n:
                 return
             if arr[row][col]==-1 or arr[row][col]=="0":
@@ -10,10 +10,10 @@ class Solution:
             
             arr[row][col]=-1
             
-            f(arr,row-1,col,flag)
-            f(arr,row,col-1,flag)
-            f(arr,row+1,col,flag)
-            f(arr,row,col+1,flag)
+            f(arr,row-1,col)
+            f(arr,row,col-1)
+            f(arr,row+1,col)
+            f(arr,row,col+1)
             
             flag=True
     
@@ -22,5 +22,5 @@ class Solution:
             for j in range(n):
                 if grid[i][j]=="1":
                     islands+=1
-                    f(grid,i,j,False)
+                    f(grid,i,j)
         return islands
